16c0db215b271cb96b8dcf94d8d114174f82329f
Laura-Salmonsen <balas@aarhus.dk>
2025-10-27 13:58:39 +0100

    prints

--- robot_framework/process.py
+++ robot_framework/process.py
@@ -38,12 +38,15 @@ def process(orchestrator_connection: OrchestratorConnection, queue_element: Queu
 
     orchestrator_connection.log_info(f'Variable {SagsID}, {PersonaleSagsTitel}')
     session = create_session(go_username_test, go_password_test)
+
     if Udleveringsmappelink:
         #hvis der allerede ligger en udleveringsmappe skal den slettes for ikke at have dobbeltmapper til at ligge
         UdleveringsSagsID = Udleveringsmappelink.rsplit("/")[-1]
         orchestrator_connection.log_info(f'Gammel udleveringsmappe detekteret {UdleveringsSagsID} {Udleveringsmappelink}')
         delete_case_go(gotesturl, session, UdleveringsSagsID)
         orchestrator_connection.log_info(f'Gammel delingsmappe slettet for sag {UdleveringsSagsID}')
+    else:
+        orchestrator_connection.log_info('Ingen udleveringsmappe i forvejen')
     #1 - definer sharepointsite url og mapper
     orchestrator_connection.log_info('Defining sharepoint stuff')
 
